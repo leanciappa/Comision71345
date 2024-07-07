c65510d0c3db377e530535989d123d3840985109
leanciappa <ciapparelli.leandro@gmail.com>
2024-07-07 02:15:38 -0300

    Clase0

---
+++ CLASE 0/app.js
@@ -0,0 +1,8 @@
+//Este es un comentario
+alert("Hola mundo"); //Salida con el mesaje Hola mundo
+
+//Variable
+let mensaje = "Hola a todos, estamos en el curso de js" //Definir una variable mensaje, con un texto
+alert(mensaje); //mostrar una alerta con el contenido de la variable mensaje
+console.log("Holis") 
+console.log (mensaje)
