5997d8019a0083108a0c7b602d133eaa72c732c1
leanciappa <ciapparelli.leandro@gmail.com>
2024-07-24 01:38:29 -0300

    2

---
+++ CLASE 2/app.js
@@ -0,0 +1,12 @@
+/* if (true){ */
+/*     console.log("vas a ver este mensaje") */
+/* } */
+
+let edad = 15;
+
+if (edad >=18){
+
+    console.log("Eres mayor de edad")
+} else {
+    console.log ("eres menor de edad ")
+}
