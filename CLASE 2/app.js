b3e98131eac1923b4c718bb9fc0156f11dc520fd
leanciappa <ciapparelli.leandro@gmail.com>
2024-07-24 23:24:15 -0300

    2

--- CLASE 2/app.js
+++ CLASE 2/app.js
@@ -2,11 +2,40 @@
 /*     console.log("vas a ver este mensaje") */
 /* } */
 
-let edad = 15;
+// let edad = 15;
+// 
+// if (edad >=18){
+// 
+    // console.log("Eres mayor de edad")
+// } else {
+    // console.log ("eres menor de edad ")
+// }
 
-if (edad >=18){
+/* let edad = prompt ("Ingrese su edad") */
+/* if (edad >= 18){ */
+/*     alert("Usted es mayor de edad") */
+/* } else { */
+/*     alert ("Usted es menor de edad") */
+/* } */     
 
-    console.log("Eres mayor de edad")
-} else {
-    console.log ("eres menor de edad ")
-}
+
+/* let nombreDeUsuario = prompt ("Ingrese el nombre del usuario") */
+/*  */
+/* if (nombreDeUsuario==""){ */
+/*     alert("No ingresó el nombre de usuario")     */
+/* } else{ */
+/*     alert("Nombre de usuario ingresado: "+nombreDeUsuario) */
+/* } */
+
+
+/* let numero = prompt ("Ingrese un número") */
+/*  */
+/* if (numero<=10){ */
+/*     alert ("El número ingresado es menor a 10") */
+/* }else if (numero<20){ */
+/*     alert ("el número ingresado es mayor a 10 pero menor a 20" ) */
+/* } else { */
+/*     alert ("El número es mayor a 20") */
+/* } */
+
+
