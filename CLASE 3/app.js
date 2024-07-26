160dc6f1398094d8e5bd61e340b3dc5eb1bfff07
leanciappa <ciapparelli.leandro@gmail.com>
2024-07-26 00:48:04 -0300

    v3

---
+++ CLASE 3/app.js
@@ -0,0 +1,56 @@
+// for
+
+/* alert ("Inicio") */
+/*  */
+/* for(let i=0; i<10; i++){ */
+/*     alert(i) */
+/* } */
+/*  */
+/* alert ("fin") */
+
+
+// Ejemplo de iteración e impresion de la tabla completa en el alert
+/* let numero = parseInt(prompt ("Ingrese el número a multiplicar")) */
+/* let impresion2 ="" */
+/*  */
+/* for(let i=0; i<10; i++){ */
+/*     let resultado = numero*i */
+/*     impresion2 = impresion2 + numero + "X"+i+"="+resultado+"\n" */
+/* } */
+/*  */
+/* alert(impresion2) */
+
+
+//Sentencia Break
+
+// let numero = parseInt(prompt ("Ingrese el número a multiplicar"))
+// let impresion2 =""
+// 
+// for(let i=0; i<10; i++){ 
+    // let resultado = numero*i 
+    // impresion2 = impresion2 + numero + "X"+i+"="+resultado+"\n"
+    // if (i==5){
+        // break;
+    // }
+// } 
+
+// alert(impresion2)
+
+
+//Sentencia Continue
+
+let numero = parseInt(prompt ("Ingrese el número a multiplicar"))
+let impresion2 =""
+
+for(let i=0; i<10; i++){ 
+    if (i==5){
+        continue;
+    }
+    let resultado = numero*i 
+    impresion2 = impresion2 + numero + "X"+i+"="+resultado+"\n"
+    
+    
+    
+} 
+
+alert(impresion2)
