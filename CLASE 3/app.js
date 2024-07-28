4a5e92c58971a92e5d6c4624b2c85b8bf7706121
leanciappa <ciapparelli.leandro@gmail.com>
2024-07-28 01:48:02 -0300

    V3

--- CLASE 3/app.js
+++ CLASE 3/app.js
@@ -39,18 +39,38 @@
 
 //Sentencia Continue
 
-let numero = parseInt(prompt ("Ingrese el número a multiplicar"))
-let impresion2 =""
+// let numero = parseInt(prompt ("Ingrese el número a multiplicar"))
+// let impresion2 =""
+// 
+// for(let i=0; i<10; i++){ 
+    // if (i==5){
+        // continue;
+    // }
+    // let resultado = numero*i 
+    // impresion2 = impresion2 + numero + "X"+i+"="+resultado+"\n"
+    // 
+    // 
+    // 
+// } 
+// 
+// alert(impresion2)
 
-for(let i=0; i<10; i++){ 
-    if (i==5){
-        continue;
-    }
-    let resultado = numero*i 
-    impresion2 = impresion2 + numero + "X"+i+"="+resultado+"\n"
-    
-    
-    
-} 
 
-alert(impresion2)
+//While (chiclo condicional, siempre y cuando se cumpla la condición se ejecutará permanentemente)
+
+// let ingresar = prompt("Ingrese un nombre").toUpperCase()
+// 
+// while (ingresar != "ESC"){
+    // alert("Usted ingresó: "+ingresar)
+    // ingresar = prompt ("Ingrese nuevamente un nombre").toUpperCase()
+// }
+//
+
+
+//DO-WHILE
+
+let ingresar
+
+do{
+    ingresar = parseInt(prompt ("Ingresar un nombre"))
+} while (ingresar >1 && ingresar<5)
